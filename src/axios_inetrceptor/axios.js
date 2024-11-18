ace8ce52dc0eb1442728b1929c8592a032567a73
anasask2001 <anas.p8113@gmail.com>
2024-11-18 14:51:21 +0530

    "update"

--- src/axios_inetrceptor/axios.js
+++ src/axios_inetrceptor/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:4600/api" 
+  baseURL: import.meta.env.VITE_REACT_APP_API_URL || "https://cart-backend-w0hz.onrender.com//api" 
 });
 
 export default axiosInstance;
